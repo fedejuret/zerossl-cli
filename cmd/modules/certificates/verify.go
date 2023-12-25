424c7b915b0af6f090b19e08e051f05eeef9410f
Federico Juretich <fedejuret@gmail.com>
2023-12-25 15:06:55 -0300

    Improvements on verify command

--- cmd/modules/certificates/verify.go
+++ cmd/modules/certificates/verify.go
@@ -4,13 +4,17 @@ Copyright © 2024 Federico Juretich <fedejuret@gmail.com>
 package certificates
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"reflect"
 
 	"github.com/fatih/color"
 	"github.com/fedejuret/zerossl-golang-cli/lib/api"
 	"github.com/fedejuret/zerossl-golang-cli/lib/api/structs/requests"
+	"github.com/fedejuret/zerossl-golang-cli/lib/models"
 	certificate_service "github.com/fedejuret/zerossl-golang-cli/lib/services"
+	"github.com/fedejuret/zerossl-golang-cli/lib/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -40,16 +44,52 @@ var verifyCmd = &cobra.Command{
 					ValidationMethod:  "HTTP_CSR_HASH",
 					VerificationEmail: "",
 				}
-			} else if verificationMethod == 2 {
+			} else if verificationMethod == 2 { // dns add cname record
 				verificationRequest = requests.VerifyDomainStructure{
 					ValidationMethod:  "CNAME_CSR_HASH",
 					VerificationEmail: "",
 				}
 			}
 
+			spinner := utils.GetSpinner("Executing validation...", "fgYellow")
+			spinner.Start()
 			response := api.Post("/certificates/"+certId+"/challenges", verificationRequest)
+			spinner.Stop()
 
-			fmt.Println(string(response))
+			var responseJson map[string]interface{}
+
+			if err := json.Unmarshal(response, &responseJson); err != nil {
+				log.Fatal(err)
+			}
+
+			success, successExists := responseJson["success"]
+			if successExists {
+				if reflect.TypeOf(success).Kind() == reflect.Bool && success == false {
+					errorInfo, errorExists := responseJson["error"].(map[string]interface{})
+					if errorExists {
+						errorMessage, typeExists := errorInfo["type"].(string)
+						if typeExists {
+							fmt.Println(color.RedString("Error when trying to verify domain: " + errorMessage))
+						} else {
+							fmt.Println(color.RedString("Error when trying to verify domain: unknown error type"))
+						}
+					} else {
+						fmt.Println(color.RedString("Error when trying to verify domain: unknown error information"))
+					}
+
+				}
+			} else {
+
+				certificate, err := models.UnmarshalCertificate(response)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				fmt.Println(color.GreenString("SUCCESS! The domain"), certificate.CommonName, color.GreenString("was validated successfully"))
+				fmt.Println(color.HiMagentaString("Now you must wait a few minutes until the entity issues the certificate to be able to download it and upload it to your site"))
+
+			}
 
 		}
 	},
